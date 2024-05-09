90e10f7a15d3facd47ded0be6a0e56614e3e1bd6
Ian C. <108159253+ic-dev21@users.noreply.github.com>
2024-05-08 22:18:15 -0400

    Update managers.py

--- custom_components/hilo/managers.py
+++ custom_components/hilo/managers.py
@@ -24,44 +24,6 @@ class UtilityManager:
         self.meter_configs = OrderedDict()
         self.meter_entities = {}
         self.new_entities = 0
-        self.get_entity_registry(hass)
-
-    def get_entity_registry(self, hass):
-        entity_registry_dict = {}
-
-        # ic-dev21 on interroge le entity registry de hass.data ici:
-        registry = hass.data.get("entity_registry")
-
-        # ic-dev21 on gère le cas d'un registry vide
-        if registry is None:
-            return entity_registry_dict
-
-        # ic-dev21: on va chercher le nom, peut-être qu'on devrait pogner la platform pour ramasser tout hilo?
-        for entity_id, entity_entry in registry.entities.items():
-            entity_registry_dict[entity_id] = {
-                "name": entity_entry.entity_id,
-                "unit_of_measurement": entity_entry.unit_of_measurement,
-            }
-
-        # ic-dev21 je trie le résultat, pourrait probablement être enlevé mais facilite la lecture en debug
-        sorted_entity_registry_dict = OrderedDict(sorted(entity_registry_dict.items()))
-        LOG.debug(f"Hil0 Ordered dict is {sorted_entity_registry_dict}")
-
-        # ic-dev21 on va chercher juste les hilo_energy, étape peut-être superflue?
-        # ici j'initialise le dict vide
-        self.filtered_entity_dict = {}
-
-        # ic-dev21 je sors tout ce qui a hilo_energy dedans
-        # ic-dev21 (NOTE): peut-être faire un append avec les low/medium
-        for entity_id, entity_data in sorted_entity_registry_dict.items():
-            if "hilo_energy" in entity_data["name"]:
-                self.filtered_entity_dict[entity_id] = entity_data
-        LOG.debug(f"Hil0 Filtered entity dict is {self.filtered_entity_dict}")
-
-        return (
-            sorted_entity_registry_dict,
-            self.filtered_entity_dict,
-        )
 
     def add_meter(self, entity, tariff_list, net_consumption=False):
         self.add_meter_entity(entity, tariff_list)
@@ -70,7 +32,7 @@ class UtilityManager:
     def add_meter_entity(self, entity, tariff_list):
         if entity in self.hass.data.get("utility_meter_data", {}):
             LOG.debug(f"Entity {entity} is already in the utility meters")
-            return
+            return None
         self.new_entities += 1
         for tarif in tariff_list:
             name = f"{entity}_{self.period}"
@@ -81,26 +43,32 @@ class UtilityManager:
                 "name": meter_name,
                 "tariff": tarif,
             }
+            return meter_name
 
     def add_meter_config(self, entity, tariff_list, net_consumption):
-        name = f"{entity}_{self.period}"
-        LOG.debug(
-            f"Creating UtilityMeter config: {name} {tariff_list} (Net Consumption: {net_consumption})"
-        )
-        self.meter_configs[entity] = OrderedDict(
-            {
-                "source": f"sensor.{entity}",
-                "name": name,
-                "cycle": self.period,
-                CONF_TARIFFS: tariff_list,
-                "net_consumption": net_consumption,
-                "utility_meter_sensors": [],
-                "offset": timedelta(0),
-                "delta_values": False,
-                "periodically_resetting": True,
-                "always_available": True,
-            }
-        )
+        meter_name = self.add_meter_entity(entity, tariff_list)
+        if meter_name:
+            name = f"{entity}_{self.period}"
+            LOG.debug(
+                f"Creating UtilityMeter config: {name} {tariff_list} (Net Consumption: {net_consumption})"
+            )
+            self.meter_configs[entity] = OrderedDict(
+                {
+                    "source": f"sensor.{entity}",
+                    "name": name,
+                    "unique_id": f"id.{meter_name}",
+                    "cycle": self.period,
+                    CONF_TARIFFS: tariff_list,
+                    "net_consumption": net_consumption,
+                    "utility_meter_sensors": [],
+                    "offset": timedelta(0),
+                    "delta_values": False,
+                    "periodically_resetting": True,
+                    "always_available": True,
+                }
+            )
+        else:
+            LOG.debug("Failed to add meter entity.")
 
     async def update(self, async_add_entities):
         LOG.debug(f"Setting up UtilityMeter entities {UTILITY_DOMAIN}")
